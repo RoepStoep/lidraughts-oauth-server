3d67fe8d4c9cac5a990e45f2923084e31117c1ea
Thibault Duplessis <thibault.duplessis@gmail.com>
2018-04-30 05:09:07 +0200

    no need for fat JWT tokens

--- src/App/OAuth/Entity/AccessToken.php
+++ src/App/OAuth/Entity/AccessToken.php
@@ -6,10 +6,16 @@ use League\OAuth2\Server\Entities\AccessTokenEntityInterface;
 use League\OAuth2\Server\Entities\Traits\AccessTokenTrait;
 use League\OAuth2\Server\Entities\Traits\EntityTrait;
 use League\OAuth2\Server\Entities\Traits\TokenEntityTrait;
+use League\OAuth2\Server\CryptKey;
 
 final class AccessToken implements AccessTokenEntityInterface
 {
     use AccessTokenTrait;
     use EntityTrait;
     use TokenEntityTrait;
+
+    public function convertToJWT(CryptKey $privateKey)
+    {
+        return $this->getIdentifier();
+    }
 }
